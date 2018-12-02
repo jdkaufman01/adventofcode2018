89e992696e565edd53459a7918a068045b423ff8
Kaufman <jdkaufman@home.ku.edu>
2018-12-02 11:18:00 -0600

    day1 pt2 solution attempt 2

--- jdkaufman/Day1/Day1pt2.ps1
+++ jdkaufman/Day1/Day1pt2.ps1
@@ -1,14 +1,23 @@
 $changes = Get-Content C:\Users\jdkaufman\GitHub\adventofcode2018\jdkaufman\Day1\input.txt 
 
+$changes = +3, +3, +4, -2, -4
+
 $start = 0 
 
 $result = $start 
 
 $i = 0
 
+$resulthash = @{}
+
 Do{
 
-    If($i -lt $changes.length){
+    $result += $changes[$i]
+
+    $resulthash["$result"] += 1
+
+    
+    If($i -lt ($changes.length -1)){
 
         Write-Output $i
 
@@ -17,19 +26,11 @@ Do{
     }
     Else{$i = 0}  
 
-    $result += $changes[$i]
-
-    If( $resultarray.contains($result) -eq $false) {
-
-        [array]$resultarray += $result  
-
-    }
-    Else{
-
-        $stop = $true 
-
-    }
-    
 
 }
-Until($stop)
+Until(($resulthash.values | Sort-Object -unique) -ne 1)
+
+$value = $($resulthash.Keys.Where({$resulthash[$_] -eq 2}))
+
+Write-Output "Duplicate found at - "$value 
+
